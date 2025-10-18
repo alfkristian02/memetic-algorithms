e491ab34b3d6a09296be632a9b16997c842ab55b
alfkristian02 <alfkristian02@gmail.com>
2025-10-18 21:04:15 +0200

    feat: add config param to save run or not

--- config.jl
+++ config.jl
@@ -1,13 +1,15 @@
 module ConfigParameters
-    export population_size, number_of_features, number_of_generations, mutation_rate, dataset_file_name, local_search_frequency, local_search_depth
+    export population_size, number_of_features, number_of_generations, mutation_rate, dataset_file_name, local_search_frequency, local_search_depth, save_run
     
-    # The focus of project assignment:
+    # The focus of the project:
     local_search_frequency::Int = 1  # how often to perform local search
     local_search_depth::Int = 1      # will be the hamming distance, at least for now
 
     population_size::Int = 500
     number_of_features::Int = 19
-    number_of_generations::Int = 100000
+    number_of_generations::Int = 100
     mutation_rate::Float64 = 0.2
     dataset_file_name::String = "10-hepatitis_dt_matG.jld2" # 01-seeds_dt_mat-1, 04-glass_dt_mat-1, 05-heart-c_dt_mat-1, 10-hepatitis_dt_matG
+
+    save_run::Bool = 1
 end
