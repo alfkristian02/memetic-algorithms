2b728f2bbd9a61bda60311d3a37b4c7f288878c4
alfkristian02 <alfkristian02@gmail.com>
2026-01-20 15:01:39 +0100

    chore: gitignore and params

--- config.jl
+++ config.jl
@@ -1,19 +1,17 @@
 module ConfigParameters
-    export population_size, number_of_generations, mutation_rate, dataset_file_name, local_search_frequency, local_search_depth, save_run, crossover_probability, sls_p
+    export EPSILON, population_size, number_of_generations, mutation_rate, local_search_frequencies, local_search_depths, save_run, crossover_probability, sls_p
     
-    # The focus of the project:
-    # local_search_frequency::Vector{Int} = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 25, 50, 100, 500, 1000]
-    local_search_frequency::Int = 1
-    # local_search_depth::Int = 1
-    local_search_depth::Vector{Int} = [1, 2, 3, 4, 5]
+    const EPSILON::Float64 = 0.001
 
-    population_size::Int = 500
-    number_of_generations::Int = 100000
-    crossover_probability::Float64 = 0.5
-    mutation_rate::Float64 = 0.1
-    dataset_file_name::String = "10-hepatitis_dt_matG.jld2" # 01-seeds_dt_mat-1, 04-glass_dt_mat-1, 05-heart-c_dt_mat-1, 10-hepatitis_dt_matG
+    local_search_frequencies::Vector{Int} = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100, 150, 250, 500]
+    local_search_depths::Vector{Int} = [1, 2, 3, 4, 5]
 
-    save_run::Bool = 1
+    population_size::Int = 250
+    number_of_generations::Int = 10000
+    crossover_probability::Float64 = 0.25
+    mutation_rate::Float64 = 0.05
+
+    save_run::Bool = 0
 
     sls_p::Float64 = 0.1
 end
