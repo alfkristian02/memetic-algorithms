3eda3572ab26d0a7e36ea33509ec0546c84b3106
alfkristian02 <alfkristian02@gmail.com>
2026-02-10 17:01:18 +0100

    refactor: closes #2

--- config.jl
+++ config.jl
@@ -1,13 +1,24 @@
 module ConfigParameters
-    export EPSILON, population_size, number_of_generations, mutation_rate, local_search_frequencies, local_search_depths, save_run, crossover_probability, sls_p
+    export dataset_file_name, fitness_function, population_size, number_of_generations, mutation_rate, local_search_frequencies, local_search_depths, save_run, crossover_probability, sls_p
+    
+    const dataset_file_name::String = "05-heart-c_dt_mat-1.jld2" # 05-heart-c_dt_mat-1.jld2 || 07-credit-a_dt_matG.jld2 || 10-hepatitis_dt_matG.jld2
     
     const EPSILON::Float64 = 0.001
 
-    local_search_frequencies::Vector{Int} = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100, 150, 250, 500]
-    local_search_depths::Vector{Int} = [1, 2, 3, 4, 5]
+    function fitness_function(base_fitness_vector::Vector{Float64}, individual_binary::BitVector, individual_decimal::Int)::Float64
+        if individual_decimal == 0
+            return .0
+        end
+        
+        return base_fitness_vector[individual_decimal] - EPSILON * sum(individual_binary)
+    end
+
+    # local_search_frequencies::Vector{Int} = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100, 150, 250, 500]
+    local_search_frequencies::Vector{Float64} = [.0, 1.0]
+    local_search_depths::Vector{Int} = [1]
 
     population_size::Int = 250
-    number_of_generations::Int = 10000
+    number_of_generations::Int = 5
     crossover_probability::Float64 = 0.25
     mutation_rate::Float64 = 0.05
 
--- main.jl
+++ main.jl
@@ -9,7 +9,7 @@ include("utils/first_try.jl")
 include("utils/SGA.jl")
 include("utils/local_search.jl")
 
-using .ConfigParameters: population_size, number_of_generations, mutation_rate, local_search_frequencies, local_search_depths, save_run, crossover_probability, sls_p, EPSILON
+using .ConfigParameters: dataset_file_name, fitness_function, population_size, number_of_generations, mutation_rate, local_search_frequencies, local_search_depths, save_run, crossover_probability, sls_p
 using .GetFitnessPool: get_precomputed_fitness_pool
 using .BinaryDecimalConversion: binary_to_decimal, decimal_to_binary
 using .SGA: sga
@@ -19,23 +19,15 @@ const dataset_file_name::String = "05-heart-c_dt_mat-1.jld2" # 05-heart-c_dt_mat
 
 println("Starting computation...")
 
-const load_base_fitness::Vector{Float64} = get_precomputed_fitness_pool(joinpath(@__DIR__, "data/precomputed_tables/", dataset_file_name))
+const base_fitness::Vector{Float64} = get_precomputed_fitness_pool(joinpath(@__DIR__, "data/precomputed_tables/", dataset_file_name))
 
-function fitness_function(individual)::Float64
-    decimal_representation = binary_to_decimal(BitVector(individual))
-    
-    if decimal_representation == 0
-        return .0
-    end
-    
-    base_fitness = load_base_fitness[decimal_representation]
-    penalty = sum(individual)
-    
-    return base_fitness - EPSILON * penalty
+function fitness_function_wrapper(individual_binary::AbstractVector{Bool})::Float64
+    individual_copy::BitVector = BitVector(individual_binary)
+    return fitness_function(base_fitness, individual_copy, binary_to_decimal(individual_copy))
 end
 
-const number_of_features::Int = log2(1+length(load_base_fitness))
-const global_optima::Float64 = fitness_function(decimal_to_binary(findmax(load_base_fitness)[2], number_of_features))
+const number_of_features::Int = log2(1+length(base_fitness))
+const global_optima::Float64 = fitness_function_wrapper(decimal_to_binary(findmax(base_fitness)[2], number_of_features))
 
 const timestamp = Dates.format(now(), "mmddHHMM")
 const filename = joinpath(@__DIR__, "runs", "main_" * dataset_file_name * "_" * timestamp * ".csv")
@@ -43,7 +35,7 @@ const filename = joinpath(@__DIR__, "runs", "main_" * dataset_file_name * "_" * 
 for i in eachindex(local_search_frequencies)
     for j in eachindex(local_search_depths)
         for _ in 1:100
-            best_individual, best_fitness, history, fitness_function_accesses, diversity = sga(population_size, number_of_features, number_of_generations, fitness_function, crossover_probability, mutation_rate, save_run, local_search_frequencies[i], local_search_depths[j], sls_p, global_optima, SLS)
+            best_individual, best_fitness, history, fitness_function_accesses, diversity = sga(population_size, number_of_features, number_of_generations, fitness_function_wrapper, crossover_probability, mutation_rate, save_run, local_search_frequencies[i], local_search_depths[j], sls_p, global_optima, SLS)
 
             if save_run
                 df = DataFrame(
